62b65a8948bb10119b396b52d890924fa2923e78
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-12-28 23:17:33 +0200

    basic structure

---
+++ firebase/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="grid place-content-center pt-80">
+      <h1 className="bg-slate-800 text-white p-4 cursor-pointer rounded-3xl">Hello world From Firebase</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ firebase/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAM00oUOAdr9qywr-3efcgVi9mtUWOzFl4",
+  authDomain: "fir-9bb34.firebaseapp.com",
+  projectId: "fir-9bb34",
+  storageBucket: "fir-9bb34.firebasestorage.app",
+  messagingSenderId: "549496057195",
+  appId: "1:549496057195:web:7269dd8d9b761c5befb02e",
+  measurementId: "G-SR9YS14J5B"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
